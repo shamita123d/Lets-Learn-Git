e8c5ae1217e2a9bfea6c313a4a164875f0ed9f84
Shamita deogade <shamitadeogade93@gmail.com>
2024-07-07 19:19:33 +0530

    added msg in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum {
     public static void main(String[] args) {
         int a=4;
         int b=3;
-        System.out.println(a+b);
+        System.out.println("sum"+(a+b));
     }
 }
