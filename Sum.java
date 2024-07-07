6c99d18422865c757ba13d48e68c7b52107375f6
Shamita deogade <shamitadeogade93@gmail.com>
2024-07-07 19:07:43 +0530

    Adding and commiting

--- Sum.java
+++ Sum.java
@@ -1,8 +1,8 @@
 public class Sum {
 
     public static void main(String[] args) {
-        int a=5 ;
-        int b=6;
+        int a=4;
+        int b=3;
         System.out.println(a+b);
     }
 }
