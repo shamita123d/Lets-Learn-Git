f426ef71dde744733a357a51c1cc011656eb4016
Shamita_Deogade <130579342+shamita123d@users.noreply.github.com>
2025-03-02 16:20:04 +0530

    Update BvToDv.java

--- BvToDv.java
+++ BvToDv.java
@@ -20,3 +20,32 @@ System.out.println("The conversion of BV to DV can be given as :: ");
     System.out.println(decimal);
     }
 }
+
+
+// import java.util.*;
+
+// class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // Taking decimal input
+//         System.out.println("Enter the Decimal Value ::");
+//         int n = sc.nextInt();
+
+//         // Decimal to Binary Conversion
+//         String binary = Integer.toBinaryString(n);
+//         System.out.println("The Conversion of DV to Binary Value is :: " + binary);
+
+//         // Toggle binary (1->0, 0->1)
+//         String toggledBinary = "";
+//         System.out.println("After Toggling :: ");
+//         for (int i = 0; i < binary.length(); i++) {
+//             toggledBinary += (binary.charAt(i) == '0') ? '1' : '0';  // Flip 0 to 1 & 1 to 0
+//         }
+//         System.out.println(toggledBinary);
+
+//         // Binary to Decimal Conversion (after toggle)
+//         int decimal = Integer.parseInt(toggledBinary, 2);
+//         System.out.println("The Conversion of Toggled BV to DV is :: " + decimal);
+//     }
+// }
